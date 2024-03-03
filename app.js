b1d636dd87d2060c0a50c538f2801a354c938641
Shamimul143 <shamimul143@gmail.com>
2024-03-04 00:26:23 +0600

    condition update done

--- app.js
+++ app.js
@@ -4,16 +4,25 @@ const allPost = async (input) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${input}`)
     const data = await res.json()
 
-
     data.posts.forEach((item) => {
+        //------------------------------------------------------
+        let verified = ""
+        if (item.isActive) {
+            verified = `<img class="absolute left-[26px] lg:left-[64px]" src="./images/Status.png" alt="">`
+        } else {
+            verified = `<img class="absolute left-[26px] lg:left-[64px]" src="./images/Status2.png" alt="">`
+        }
+        // ---------------------------------------------------------------
         document.getElementById("loading-spinner").classList.add("hidden")
+
         const div = document.createElement("div")
+
         div.classList = ("col-span-2")
         div.innerHTML = `
         <div id="post-container">
         <div class="flex  bg-[#797dfc1a] rounded-lg p-4 gap-2 mb-4">
             <div>
-                <img class="absolute left-[26px] lg:left-[64px]" src="./images/Status.png" alt="">
+                ${verified}
                <div> <img class="rounded-xl w-[72px]" src="${item.image}" alt=""></div>
             </div>
             <div>
@@ -35,11 +44,8 @@ const allPost = async (input) => {
             </div>
         </div>
     </div>
-`
-
+    `
         forumContainer.appendChild(div)
-
-
     })
 }
 
@@ -52,52 +58,51 @@ function searchBtn() {
 }
 
 // ------------------------------------------------
-
+let count1 = 1;
 const titleContainer = document.getElementById("title-container")
-const titleBtn = (a, b=0) => {
 
+const titleBtn = (a, b = 0) => {
+    document.getElementById("count").innerText = count1++
     const div = document.createElement("div")
-    div.classList = ("flex justify-between")
+    div.classList = ("flex justify-between shadow-2xl p-3 rounded-lg")
     div.innerHTML = `
-                <div class='mb-3'>
+                <div class='mb-3 '>
                 <p>${a}</p>
                  </div>
                   <div class="flex gap-2 items-center">
                    <div><img src="./images/Group16.png" alt=""></div>
                     <span>${b}</span>
                 </div>
-
 `
     titleContainer.appendChild(div)
-
-
-
-
-    
 }
 
-
-
 // ---------------------------------
-
-
 
 const latestPostCon = document.getElementById("latest-post")
 const latestPost = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/retro-forum/latest-posts")
     const data = await res.json()
-    data.forEach((item) => {
 
+    let date = ""
+    data.forEach((item) => {
+        // -----------------------------------------------------------------
+        if (item.author.posted_date) {
+            date = `<span>${item.author.posted_date}</span>`
+        } else {
+            date = "Unknown"
+        }
+        // ----------------------------------------------------------------
         const div = document.createElement("div")
         div.innerHTML = `
-
-<div class="card bg-base-100 shadow-xl">
+                <div class="card bg-base-100 shadow-xl">
                     <figure class="px-10 pt-10">
                         <img src="${item.cover_image}" alt="Shoes"
                             class="rounded-xl" />
                     </figure>
                     <div class="card-body">
-                        <div class="flex gap-2 text-[#12132d99] "><img src="./images/Frame2.png" alt=""><span>${item.author.posted_date}</span>
+                        <div class="flex gap-2 text-[#12132d99] "><img src="./images/Frame2.png" alt="">
+                        ${date}
                         </div>
                         <h2 class="card-title font-bold">${item.title}</h2>
                         <p class="text-[#12132d99]">${item.description} </p>
@@ -110,22 +115,10 @@ const latestPost = async () => {
                         </div>
                     </div>
                 </div>
-
 `
         latestPostCon.appendChild(div)
-
-
-
-
     })
 }
 
-
 latestPost()
-
-
-
-
-
-
 allPost('Comedy')
